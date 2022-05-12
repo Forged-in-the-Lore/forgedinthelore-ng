ed83a73c71af4d38c7f73fc3c4fae66d78599205
David Hellinga <d.hellinga@student.fontys.nl>
2022-05-12 13:06:54 +0200

    Added JWT interceptor to resolve #320. Added interceptor and some material components to module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {NavComponent} from './nav/nav/nav.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {ErrorInterceptor} from "./_interceptors/error.interceptor";
+import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
+import {LoadingInterceptor} from "./_interceptors/loading.interceptor";
+import {MatSpinnerOverlayComponent} from './mat-spinner-overlay/mat-spinner-overlay/mat-spinner-overlay.component';
+import {HasRoleDirective} from './_directives/has-role.directive';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MatSpinnerOverlayComponent
+    // HasRoleDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [
+    // {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    // {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
